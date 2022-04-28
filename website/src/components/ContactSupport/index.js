68b93a8ca4ac2af4cf373597e8309414e79858c7
Erez Shtayman <73282357+erezstmn-doit@users.noreply.github.com>
2022-04-28 13:45:45 +0000

    Add contact support component

---
+++ website/src/components/ContactSupport/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+const ContactSupport = () => {
+  return (
+    <div className={styles.contact}>
+      <h1 className={styles.contact_title}>Contact support</h1>
+      <p>
+        If you need additional support, one of our knowledgeable support team
+        will be happy to assist.
+      </p>
+      <button href="#" className={styles.contact_button}>
+        raise a ticket
+      </button>
+    </div>
+  );
+};
+
+export default ContactSupport;
