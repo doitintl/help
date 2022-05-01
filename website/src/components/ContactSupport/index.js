dd2c32354a215398d52c364196d06de055239ff4
Erez Shtayman <73282357+erezstmn-doit@users.noreply.github.com>
2022-05-01 09:25:06 +0000

    Fix knowledgeable typo

--- website/src/components/ContactSupport/index.js
+++ website/src/components/ContactSupport/index.js
@@ -6,7 +6,7 @@ const ContactSupport = () => {
     <div className={styles.contact}>
       <h1 className={styles.contact_title}>Contact support</h1>
       <p>
-        If you need additional support, one of our knowledgeble support team
+        If you need additional support, one of our knowledgeable support team
         will be happy to assist.
       </p>
       <button href="#" className={styles.contact_button}>
