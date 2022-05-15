3e54d7a59680778dbe741bcde3d1091328105b9b
Naomi Rose <23469+nomiro@users.noreply.github.com>
2022-05-15 11:31:12 +0000

    Temporarily disable the DocSearch component

--- website/src/components/Search/index.js
+++ website/src/components/Search/index.js
@@ -4,11 +4,15 @@ import { DocSearch } from '@docsearch/react';
 import '@docsearch/css';
 
 const Search = () => (
+  // Disabled because it causes the index page to crash
+  /*
   <DocSearch
     appId={process.env.ALGOLIA_APP_ID}
     indexName={process.env.INDEX_NAME}
     apiKey={process.env.ALGOLIA_API_KEY}
   />
+  */
+  <div />
 );
 
 export default Search;
