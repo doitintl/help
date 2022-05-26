81c769c180c527005c5a59669b364e8dfcf68e97
Naomi <23469+norosa@users.noreply.github.com>
2022-05-26 12:10:51 +0000

    Configure the initial set of spotlight features
    Spotlight features:
    
    - Cloud analytics
    - Rightsizing for Google Cloud
    - Flexsave
    - BigQuery Lens
    - AWS Lens
    - Spot scaling
    
    I assembled this initial list of spotlight features to mirror the
    existing docs as much as possible by:
    
    - Cherry-picking prominent headings from the CMP docs front page and
      navigation sidebar.
    
    - Ordering them approximately by their order of appearance in the
      aforementioned places.
    
    The CMP docs front page and navigation sidebar are in need of some major
    improvements, but that is out of scope for now.
    
    To make the transition away from GitBook as smooth as possible, we want
    to launch a Docusaurus website that closely mirrors the existing CMP
    docs. After launch, we can restructure and rework what we have as we
    develop out the help center.
    
    Over time, we can use analytics (e.g., Mixpanel) and internal feedback
    to improve (and maybe even automate) our selections for the spotlight
    features.

--- website/src/components/IconCard/index.js
+++ website/src/components/IconCard/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ThemedImage from '@theme/ThemedImage';
+import Link from '@docusaurus/Link';
 
 import styles from './styles.module.css';
 
 const IconCard = ({ card }) => (
   <div className={`col ${styles.icon_card}`}>
-    <a href={card.href} className={styles.icon_card_inner}>
+    <Link to={card.href} className={styles.icon_card_inner}>
       <card.image />
       <p className={styles.icon_card_title}>{card.title}</p>
-    </a>
+    </Link>
   </div>
 );
 
