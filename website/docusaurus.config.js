afb8169a2c3fd1629a57c7c3c599818781b464ff
Naomi <23469+norosa@users.noreply.github.com>
2022-06-03 22:33:15 +0000

    Add announcement bar for Pride Month

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -51,6 +51,16 @@ const config = {
         disableSwitch: true,
         respectPrefersColorScheme: false,
       },
+      announcementBar: {
+        id: 'pride',
+        content:
+          '&#x1f3f3;&#xfe0f;&#x200d;&#x1f308; ' +
+          'Celebrate Pride Month with DoiT: <wbr> Join our ' +
+          '<a href="https://resources.doit-intl.com/pride-at-doit-2022">' +
+          'Speaker Session with Briona Jenkins</a> on June 14th ' +
+          '&#x1f3f3;&#xfe0f;&#x200d;&#x1f308;',
+        isCloseable: false,
+      },
       navbar: {
         title: 'Help Center',
         hideOnScroll: true,
