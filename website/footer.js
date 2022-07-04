ada8abd1ebd2edfa1402af246da13803ee59dd59
Naomi <23469+norosa@users.noreply.github.com>
2022-07-04 14:17:37 +0000

    Update branding
    This commit updates the Help Center branding using the new branding
    assets.
    
    Specifically:
    
    - New favicons
    - New icons for cross-browser PWA support (including [maskable
      icons][maskable])
    - New assets for Safari
    - New tiles for Microsoft Metro
    - A few fixes and improvements for other metadata definitions as I went
      about updaing the icons
    
    [maskable]: https://web.dev/maskable-icon/

--- website/footer.js
+++ website/footer.js
@@ -98,7 +98,7 @@ const footer = {
   style: 'dark',
   logo: {
     alt: doit.companyName,
-    src: 'assets/images/doit-logo-white.svg',
+    src: '/brand/logo-white.svg',
     href: doit.homeUrl,
     width: 100,
     height: 100,
