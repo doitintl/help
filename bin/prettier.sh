f46283d9eba9e30a2f0170c7baa7b692fe08af18
Naomi <23469+norosa@users.noreply.github.com>
2022-07-20 09:31:17 +0000

    Add `--dry-run` opiion to `prettier` check
    Previously, when you run `make check`, it would run:
    
    ```
    make prettier
    ./bin/prettier.sh
    ```
    
    A common problem with Prettier is getting your text editor and the
    command line tool to agree on how files should be formatted. To fix
    this, I have added the ability to update files in place and added the
    `--dry-run` option for checks that should not modify files.
    
    Now, `make check` will run:
    
    ```
    make prettier-dry
    ./bin/prettier.sh --dry-run
    ```
    
    If you're experiencing problems trying to format the files correctly to
    Prettier's liking, you can now run:
    
    ```
    make prettier
    ./bin/prettier.sh
    .cspell.json 47ms
    .devcontainer/devcontainer.json 4ms
    .docops/algolia/template.json 8ms
    .github/dependabot.yml 29ms
    .github/mergify.yml 8ms
    .github/workflows/codeql.yml 15ms
    .github/workflows/cron.yaml 33ms
    .github/workflows/deployment.yaml 22ms
    .github/workflows/integration.yaml 23ms
    .github/workflows/review.yaml 27ms
    [...]
    ```
    
    As you can see above, Prettier is now going through each file and making
    the necessary changes.

--- bin/prettier.sh
+++ bin/prettier.sh
@@ -3,7 +3,7 @@
 # Wrap the `prettier` command to provide configuration
 # =============================================================================
 
-# Usage: ./bin/prettier.sh
+# Usage: ./bin/prettier.sh [-d|--dry-run]
 
 # https://github.com/prettier/prettier
 
@@ -15,9 +15,28 @@ export LC_ALL
 RED='\x1b[1;31m'
 RESET='\x1b[0m'
 
-tmp_errors="$(mktemp)"
-if ! prettier --check --ignore-unknown . >"${tmp_errors}" 2>&1; then
-    sed -E "s,^(.*),${RED}\1${RESET}," <"${tmp_errors}"
-    rm "${tmp_errors}"
-    exit 1
+dry_run=0
+for arg in "$@"; do
+    case "${arg}" in
+    -d | --dry-run)
+        dry_run=1
+        shift
+        ;;
+    -*)
+        echo "ERROR: Unknown option: ${arg}"
+        exit 1
+        ;;
+    *) ;;
+    esac
+done
+
+if test "${dry_run}" = 1; then
+    tmp_errors="$(mktemp)"
+    if ! prettier --write --ignore-unknown . >"${tmp_errors}" 2>&1; then
+        sed -E "s,^(.*),${RED}\1${RESET}," <"${tmp_errors}"
+        rm "${tmp_errors}"
+        exit 1
+    fi
+else
+    prettier --write --ignore-unknown .
 fi
